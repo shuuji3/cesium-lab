8f61124e33c1ec8e37e6420db57429e41b299213
TAKAHASHI Shuuji <shuuji3@gmail.com>
2021-09-22 12:16:54 +0900

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import token from "./token";
+import "./style.css";
+
+// Your access token can be found at: https://cesium.com/ion/tokens.
+// This is the default access token from your ion account
+
+Cesium.Ion.defaultAccessToken = token;
+
+// Initialize the Cesium Viewer in the HTML element with the `cesium-container` ID.
+const viewer = new Cesium.Viewer("cesium-container", {
+  terrainProvider: Cesium.createWorldTerrain(),
+});
+
+// Add Cesium OSM Buildings, a global 3D buildings layer.
+const buildingTileset = viewer.scene.primitives.add(Cesium.createOsmBuildings());
+
+// Fly the camera to San Francisco at the given longitude, latitude, and height.
+viewer.camera.flyTo({
+  destination: Cesium.Cartesian3.fromDegrees(-122.4175, 37.655, 400),
+  orientation: {
+    heading: Cesium.Math.toRadians(0.0),
+    pitch: Cesium.Math.toRadians(-15.0),
+  },
+});
